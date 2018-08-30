f17e44ab7d40943856277f2d53d01bf49caeca8a
satyasinha94 <satyasinha94@gmail.com>
2018-08-30 19:26:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = {Arctic_Monkeys: "Miracle Aligner"};
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
 playlist[artistName] = 'songTitle'
 return playlist
 }
@@ -7,3 +8,7 @@ function removeFromPlaylist(playlist, artistName, songTitle) {
   delete playlist[artistName]
   return playlist
 }
+=======
+return playlist
+}
+>>>>>>> 0aa601056c83fe69e51aa0cdc5c8b40f6e91d3a2
